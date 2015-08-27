d97881e452cde6a35901f58569f98e34f3f37266
mrmrs <hi@mrmrs.cc>
2015-08-27 00:44:53 -0700

    update build step to postcss

--- build.js
+++ build.js
@@ -20,17 +20,17 @@ var output = postcss([autoprefixer])
   .use(customMedia())
   .process(css, {
     from: "src/tachyons-utilities.css",
-    to: "css/tachyons-utilities.css"
+    to: "tachyons-utilities.css"
   })
   .css
 
-fs.writeFile("css/tachyons-utilities.css", output, 'utf-8')
+fs.writeFile("tachyons-utilities.css", output, 'utf-8')
 
 // Using YUI Compressor for CSS
 new compressor.minify({
     type: 'sqwish',
-    fileIn: 'css/tachyons-utilities.css',
-    fileOut: 'css/tachyons-utilities.min.css',
+    fileIn: 'tachyons-utilities.css',
+    fileOut: 'tachyons-utilities.min.css',
     callback: function(err, min){
     //console.log('Sqwish');
     //console.log(err);
